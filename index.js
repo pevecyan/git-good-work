4b3801eb0c56ec92828a11cd0294f664768fb915
Žan Pevec <pevecyan@outlook.com>
2018-02-22 00:52:20 +0100

    Asd

--- index.js
+++ index.js
@@ -6,9 +6,40 @@ let options = cli.parse();
 let cwd = process.cwd();
 
 
-exec('git status', (err, stdout, stderr)=>{
-    console.log(stdout);
-    console.log(stderr);
-    
+exec('git log', (err, stdout, stderr)=>{
+    if(stderr)
+        console.log(stderr);
+    handleOutput(stdout);
 })
 
+
+function handleOutput(output){
+    let lines = output.split('\n');
+
+    let commits = [];
+    
+    for(let i = 0; i < lines.length; i++){
+        //console.log(lines[i])
+        let line = lines[i];
+        if(line.indexOf('commit') == 0){
+            let commit = {
+                id: line.replace('commit ', ''),
+                author: lines[++i].replace('Author:', '').trim(),
+                date: lines[++i].replace('Date:', '').trim()
+            };
+            //ignore one line
+            i++;
+            let message = '';
+            for(let k = i; k < lines.length; k++, i++){
+                if(lines[k].indexOf('commit') == 0){
+                    i--;
+                    break;
+                }
+                message = message + lines[k];
+            }
+            commit.message = message.trim();
+            commits.push(commit);
+        }
+    }
+    console.log(commits);
+}
